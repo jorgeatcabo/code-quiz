3317d8a379ad137628b111b7cd2c7c53ded34105
El Roble <san_lucas2005@yahoo.com>
2021-03-23 14:05:35 -0700

    First files upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+var startButton=document.getElementById="start-button";
+var startSection=document.querySelector(".start-section");
+document.getElementById("start-button").addEventListener("click", function() {
+    startSection.style.visibility = "hidden";
+  });
+
